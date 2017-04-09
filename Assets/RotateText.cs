142b6289fe01ef60d6161bcd739ad7cd38cb42a5
laggyluk <laggyluk@gmail.com>
2017-04-09 15:28:07 +0200

    added dummy image source

---
+++ Assets/RotateText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateText : MonoBehaviour {
+
+    public float speed = 10;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+	}
+}
